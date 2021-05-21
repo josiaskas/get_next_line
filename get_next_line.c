2a3fa49714eee96055bbe65f1bb381735efbc6a1
Josias Kasongo <josiaskas@gmail.com>
2021-05-20 22:29:01 -0400

    correcting no new line end

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jkasongo <jkasongo@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 09:25:51 by jkasongo          #+#    #+#             */
-/*   Updated: 2021/05/20 19:39:10 by jkasongo         ###   ########.fr       */
+/*   Updated: 2021/05/20 22:27:39 by jkasongo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,7 +106,7 @@ int	get_next_line(int fd, char **line)
 	else
 		check = ft_strlcpy(file, tmp, (BUFFER_SIZE + 1));
 	free(tmp);
-	if (readed > 1)
+	if ((readed > 1) || ((readed == 0) && (*line != 0)))
 		return (1);
 	return (readed);
 }
